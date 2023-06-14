13307b74fec5b861ead599cbaf20bfa2429250c8
caperezr <cris71l124@hotmail.com>
2023-06-14 15:42:54 -0500

    First Commit

--- src/routes/electricity/router.py
+++ src/routes/electricity/router.py
@@ -4,4 +4,8 @@ router = APIRouter()
 
 @router.get("/all")
 def get_electricidad():
-    return {"message": "Información sobre electricidad"}
+    return {"message": "Información sobre pokemones de electricidad"}
+
+@router.get("/byname/{pokemon_name}")
+def get_pokemon_electricity_by_name(pokemon_name: str):
+    return {"message": f"Información sobre el pokemon de electricidad llamado : {pokemon_name}"}
